5c7d19e498b4081b0385ce17ab9c329bc757771e
Divya Mehra <divya.w.mehra@gmail.com>
2026-01-25 17:53:02 +0000

    color changes

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
+const rightSide = document.querySelector(".right-side");
+const leftSide = document.querySelector(".left-side");
+const root = document.documentElement;
+
 const students = {
-  0: { name: "Appollonia" },
-  1: { name: "Amulya" },
-  2: { name: "Leslie" },
-  3: { name: "Aidan" },
-  4: { name: "Emily" },
-  5: { name: "Emigdio" },
-  6: { name: "Yuhan" },
-  7: { name: "Sofia" },
-  8: { name: "Emma" },
-  9: { name: "Laney" },
-  10: { name: "Felicia" },
-  11: { name: "Ruoxi" },
-  12: { name: "Annie" },
-  13: { name: "Binbing" },
-  14: { name: "Divya" },
-  15: { name: ":)" }
-
-
+  0: { name: "Appollonia", color: "#719656" },
+  1: { name: "Amulya", color: "#163813" },
+  2: { name: "Leslie", color: "#543A2C" },
+  3: { name: "Aidan", color: "#19d1a9" },
+  4: { name: "Emily", color: "#799c58" },
+  5: { name: "Emigdio", color: "#89CFF0", textColor: "#000"  },
+  6: { name: "Cori", color: "#00ffe4", textColor: "#000" },
+  7: { name: "Sofia", color: "#550000" },
+  8: { name: "Emma", color: "#5E011C" },
+  9: { name: "Laney", color: "#d60458" },
+  10: { name: "Ruoxi", color: "#4c6a11" }, // update via email
+  11: { name: "Annie", color: "#ADD8E6", textColor: "#000"  },
+  12: { name: "Candice", color: "#87CEEB" },
+  13: { name: "Neev", color: "#F2F0EF", textColor: "#000", secondaryColor: "#e2d9be"  },
+  14: { name: "Divya", color: "#4c6a11" },
+  15: { name: ":)", color: "#4c6a11" }
 };
 
-let container = document.querySelector(".right-side");
-
-
 for (let key in students) {
-  console.log(students[key].name)
   let clock = document.createElement("div");
   clock.innerHTML = `<div class="clock">
                 <div class="hand hour"></div>
                 <div class="hand minute"></div>
                 <div class="name">${students[key].name}</div>
                 </div>`
-  container.append(clock);
+  // clock.style.color
+  clock.addEventListener('mouseenter', () => {
+    console.log(students[key].color)
+    root.style.setProperty('--primary-color', students[key].color)
 
+    if (students[key].textColor) {
+      root.style.setProperty('--text-color', students[key].textColor)
+    } else {
+      root.style.setProperty('--text-color', "#efe8d8")
 
+    }
 
+    if (students[key].secondaryColor) {
+      root.style.setProperty('--secondary-color', students[key].secondaryColor)
+    } else {
+      root.style.setProperty('--secondary-color', "#efe8d8")
+
+    }
+  })
+
+  rightSide.append(clock);
 
 }
+
+leftSide.addEventListener('mouseenter', () => {
+  root.style.setProperty('--primary-color', "#4c6a11")
+})
